7af9dc051cb8cd636bee82bb3caf18b7076a7a12
Rodolfo <rodolfogroero2@gmail.com>
2024-01-31 13:53:01 -0500

    connect db

--- database/database.js
+++ database/database.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'postgres'
+  dialect: 'mysql'
 });
 
 // Prueba la conexión
@@ -19,3 +19,4 @@ async function testConnection() {
 }
 
 testConnection();
+module.exports=sequelize;
