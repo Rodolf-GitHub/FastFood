6c9d3c40d2eedae157c72b362f723f22c9f523d8
Rodolfo <rodolfogroero2@gmail.com>
2024-01-31 13:27:17 -0500

    modifi db conection variables

--- database/database.js
+++ database/database.js
@@ -1,7 +1,21 @@
-const Sequelize = require("sequelize")
+const dotenv = require("dotenv");
+dotenv.config();
+const { Sequelize } = require('sequelize');
 
- const sequelize = new Sequelize("fastFoodDB", "postgres", "010103", {
-  host: "localhost",
-  dialect: "postgres",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres'
 });
-module.exports=sequelize;
+
+// Prueba la conexión
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión exitosa');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  }
+}
+
+testConnection();
