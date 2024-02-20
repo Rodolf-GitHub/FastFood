1e0ea23e8cdb6c4ffcf831cbbdf697bf83fb751d
Rodolfo <rodolfogroero2@gmail.com>
2024-02-20 15:39:23 -0500

    fixing import errors

--- models/userModels.js
+++ models/userModels.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../database/connection');
+const sequelize = require('../database/database');
 
 const User = sequelize.define('User', {
   username: {
