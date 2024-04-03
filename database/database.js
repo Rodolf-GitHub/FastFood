3794f089d3302708dc8dced62ff95f994c549530
Rodolfo <rodolfogroero2@gmail.com>
2024-04-03 17:53:22 -0400

    changes in .env

--- database/database.js
+++ database/database.js
@@ -6,7 +6,7 @@ const logger = require("../loggers/loggers");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'postgres'
+  dialect: process.env.DB_DIALECT
 });
 
 // Prueba la conexión
